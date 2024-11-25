7727d2dfbee2cb0aa99415263b11d3b4d6786d3e
Dana <beware.14b@gmail.com>
2024-11-25 13:24:56 +0200

    affect only perishables

--- Composter/src/BlockEntity/BlockEntityComposter.cs
+++ Composter/src/BlockEntity/BlockEntityComposter.cs
@@ -6,6 +6,9 @@ public class BlockEntityComposter : BlockEntityGenericTypedContainer
     protected override void InitInventory(Block block)
     {
         base.InitInventory(block);
-        container.Inventory.OnAcquireTransitionSpeed += (type, stack, mul) => { return Core.Config.PerishRate; };
+        container.Inventory.OnAcquireTransitionSpeed += (type, stack, mul) =>
+        {
+            return type == EnumTransitionType.Perish ? Core.Config.PerishRate : container.GetPerishRate();
+        };
     }
 }
